437d38cfc4fbee092ba08e0435ad4ce8749473cc
Siekierson <adrian.siekierko@gmail.com>
2021-03-24 14:58:12 +0100

    restructure database to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -8,6 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", function () {
+  console.log("db connect");
+});
+
 const routes = require("./routes");
 
 app.use("/", routes);
